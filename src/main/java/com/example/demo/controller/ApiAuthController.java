9a7df207dcdc6803e21c9c1e37a0ffd0eecbdb22
egor-vafin <post1978@gmail.com>
2022-07-14 12:50:15 +0300

    captcha ver 2

--- src/main/java/com/example/demo/controller/ApiAuthController.java
+++ src/main/java/com/example/demo/controller/ApiAuthController.java
@@ -2,9 +2,19 @@ package com.example.demo.controller;
 
 import com.example.demo.api.response.AuthCheckResponse;
 import com.example.demo.api.response.CaptchaResponse;
+import com.github.cage.GCage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,12 +29,47 @@ public class ApiAuthController {
         return authCheckResponse;
     }
 
-    @RequestMapping("/api/auth/captcha")
+    @RequestMapping("/captcha")
     @ResponseBody
     public CaptchaResponse captchaResponse() {
 
+        GCage gCage = new GCage();
 
-        return null;
+        String captchaText = gCage.getTokenGenerator().next();
+        String secretCode = gCage.getTokenGenerator().next() + gCage.getTokenGenerator().next() + gCage.getTokenGenerator().next();
+
+        BufferedImage image = gCage.drawImage(captchaText);
+
+        BufferedImage resImage = resize(image, 100, 35);
+
+        byte[] newImage = imgToBase64String(resImage, "png");
+
+        String encodedString = Base64.getEncoder().encodeToString(newImage);
+
+        return new CaptchaResponse("1234", "data:image/png;base64, " + encodedString);
+    }
+
+    public static byte[] imgToBase64String(final RenderedImage img, final String formatName) {
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        try {
+            ImageIO.write(img, formatName, os);
+            os.close();
+            return os.toByteArray();
+        } catch (final IOException ioe) {
+            throw new UncheckedIOException(ioe);
+        }
+    }
+
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
     }
 
 }
--- src/main/java/com/example/demo/service/CaptchaGenerator.java
+++ src/main/java/com/example/demo/service/CaptchaGenerator.java
@@ -1,12 +1,39 @@
 package com.example.demo.service;
 
 import com.example.demo.api.response.CaptchaResponse;
+import com.github.cage.GCage;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Base64;
 
 @Service
 public class CaptchaGenerator {
 
     public CaptchaResponse createCaptcha() {
+
+
+        GCage gCage = new GCage();
+
+        String captchaText = gCage.getTokenGenerator().next();
+        String secretCode = gCage.getTokenGenerator().next() + gCage.getTokenGenerator().next() + gCage.getTokenGenerator().next();
+
+        BufferedImage image = gCage.drawImage(captchaText);
+
+        BufferedImage resImage = resize(image, 100, 35);
+
+        byte[] newImage = imgToBase64String(resImage, "png");
+
+        String encodedString = Base64.getEncoder().encodeToString(newImage);
+
+        return new CaptchaResponse(secretCode, "data:image/png;base64, " + encodedString);
+
 
 
 //
@@ -17,5 +44,29 @@ public class CaptchaGenerator {
 
 
         return null;
+    }
+
+
+    public byte[] imgToBase64String(final RenderedImage img, final String formatName) {
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        try {
+            ImageIO.write(img, formatName, os);
+            os.close();
+            return os.toByteArray();
+        } catch (final IOException ioe) {
+            throw new UncheckedIOException(ioe);
+        }
+    }
+
+    public BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
     }
 }
